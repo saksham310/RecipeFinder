a279b6a44381874b271583d6e7d20ca9d6d4d0ce
saksham.s <sakshams982@gmail.com>
2024-04-19 15:01:54 +0545

    updated the detail page to dynamically dispaly values

--- src/app/detail-page/detail-page.component.ts
+++ src/app/detail-page/detail-page.component.ts
@@ -3,21 +3,34 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { ActivatedRoute } from '@angular/router';
 import { Foodrecipe } from '../foodrecipe';
 import { RecipeService } from '../recipe.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detail-page',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,CommonModule],
   templateUrl: './detail-page.component.html',
   styleUrl: './detail-page.component.css'
 })
 export class DetailPageComponent {
+
+box:string="ingredient";
 recipe:Foodrecipe|undefined;
 recipeService:RecipeService=inject(RecipeService);
 router:ActivatedRoute=inject(ActivatedRoute);
  constructor(){
   const id=Number(this.router.snapshot.params['id']);
   this.recipe=this.recipeService.getIdRecipe(id);
+  console.log(this.recipe?.ingredients);
  }
+getInstruction(instructions:string|undefined){
+  instructions = instructions?.trim().replace(/\.$/, "");
+return instructions?.split(".");
+
+
+}
+toggle(text:string){
+  this.box=text;
+}
 
 }
--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -29,7 +29,7 @@ export class RecipeService {
         description: "A savory garlic shrimp dish",
         ingredients: ["Shrimp", "Garlic", "Butter", "Parsley", "Lemon Juice"],
         name: "Garlic Shrimp",
-        instructions: "1. Peel and devein the shrimp. 2. Melt butter in a skillet over medium heat. 3. Add minced garlic and cook until fragrant. 4. Add shrimp to the skillet and cook until pink and opaque. 5. Sprinkle with chopped parsley and lemon juice before serving.",
+        instructions: "Peel and devein the shrimp.Melt butter in a skillet over medium heat.Add minced garlic and cook until fragrant.Add shrimp to the skillet and cook until pink and opaque.Sprinkle with chopped parsley and lemon juice before serving.",
         category: "Seafood",
         photo:`${this.baseUrl}`
       },{
@@ -37,7 +37,7 @@ export class RecipeService {
         description: "A classic beef stew with hearty vegetables",
         ingredients: ["Beef Stew Meat", "Carrots", "Potatoes", "Onion", "Beef Broth", "Tomato Paste", "Garlic", "Bay Leaves", "Thyme", "Salt", "Pepper"],
         name: "Beef Stew",
-        instructions: "1. Brown the beef stew meat in a large pot. 2. Add chopped onions and garlic, cooking until softened. 3. Stir in tomato paste, beef broth, bay leaves, and thyme. 4. Add chopped carrots and potatoes, simmer until vegetables are tender. 5. Season with salt and pepper to taste before serving.",
+        instructions: "Brown the beef stew meat in a large pot.Add chopped onions and garlic, cooking until softened.Stir in tomato paste, beef broth, bay leaves, and thyme.Add chopped carrots and potatoes, simmer until vegetables are tender.Season with salt and pepper to taste before serving.",
         category: "Stew",
         photo:`${this.baseUrl}`
       }
